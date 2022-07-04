99421fb495c9fbdaadae976e957bcf19637493cb
Raul Roldan <raul.ji@outlook.com>
2022-07-04 17:30:11 -0500

    added array and working on function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+let collection = [
+
+    {
+        name: "Brand new Eyes",
+        artist: "Paramore",
+        yearPublished: 2009,
+    }
+];
+//console.log('This is my collection of Records:', collection);
+function addToCollection(title, artist, yearPublished) {
+
+    collection.push();
+    return collection;
+}
+addToCollection('The black parade','My checmical romance', '2006')
+console.log('Collection2', collection);
