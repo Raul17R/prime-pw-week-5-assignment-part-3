70f686125ecc72a5213a008f0591c8af52adf480
Raul Roldan <raul.ji@outlook.com>
2022-07-04 18:27:18 -0500

    working on function added console logs to check if works

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,18 +1,25 @@
 console.log('***** Music Collection *****')
 
-let collection = [
-
-    {
-        name: "Brand new Eyes",
-        artist: "Paramore",
-        yearPublished: 2009,
-    }
-];
-//console.log('This is my collection of Records:', collection);
-function addToCollection(title, artist, yearPublished) {
-
-    collection.push();
-    return collection;
+let collection = [];
+let records = {
+    name: "Brand new Eyes",
+    artist: "Paramore",
+    yearPublished: 2009,
 }
-addToCollection('The black parade','My checmical romance', '2006')
-console.log('Collection2', collection);
+//console.log('This is my collection of Records:', collection);
+function addToCollection(title,artist,yearPublished) {
+    //records === collection;
+    console.log('Add title of the record',title, 'Add Artist',artist,'Add Year',yearPublished);
+
+    return title + artist + yearPublished;
+
+    }
+   
+
+//addToCollection('The black parade', 'My chemical romance', 2007);
+//console.table (addToCollection('The black parade', 'My chemical romance', 2007))
+console.log(addToCollection('Raul','Roldan',1995));
+//console.log(collection);
+//addToCollection('Raul', 'Roldan', 2009);
+//console.log(addToCollection());
+
