b377923e05c4813a745dcdb2e9a30f0df0b45cf8
Raul Roldan <raul.ji@outlook.com>
2022-07-04 19:24:37 -0500

    workign on part 3 showing albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,18 +8,36 @@ let records = {
 }
 //console.log('This is my collection of Records:', collection);
 function addToCollection(title,artist,yearPublished) {
-    //records === collection;
-    console.log('Add title of the record',title, 'Add Artist',artist,'Add Year',yearPublished);
-
-    return title + artist + yearPublished;
+    //console.log('Add title of the record',title, 'Add Artist',artist,'Add Year',yearPublished);
+    const newrecord={title:title, artist:artist, yearPublished:yearPublished,};
+    collection.push(newrecord);
+    
+    return newrecord;
 
     }
    
 
-//addToCollection('The black parade', 'My chemical romance', 2007);
-//console.table (addToCollection('The black parade', 'My chemical romance', 2007))
-console.log(addToCollection('Raul','Roldan',1995));
-//console.log(collection);
-//addToCollection('Raul', 'Roldan', 2009);
+addToCollection('The black parade', 'My chemical romance', 2007);
+console.log (addToCollection('Brand new eyes', 'Paramore', 2009))
+console.log(collection);
 //console.log(addToCollection());
+console.log (addToCollection('Fallen', 'Evanescence', 2003))
+console.log (addToCollection('Wehen we all fall asleep', 'Billie Eilish', 2019))
+console.log (addToCollection('Bon Jovi', 'Bon-Jovi', 1984))
+console.log (addToCollection('Paranoid', 'Black sabbath', 1970))
+//console.table(collection);
+console.log('Over all collection', collection);
 
+function showCollection(arrayM) {
+    
+    for (let i = 0; i < arrayM.length; i++) {
+        let album = arrayM[i];
+        album.title;
+        //if (arrayM[i]) {
+            //console.log(arrayM[i].title + ', ' + arrayM[i].artist + ', ' + arrayM[i].yearPublished + '');
+        //}
+        console.log(album.title);
+        
+    }
+}
+console.log(showCollection(collection));
