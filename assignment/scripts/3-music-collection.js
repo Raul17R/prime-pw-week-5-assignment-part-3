77b0bf66966b28ac1411a55c8d8b77a014dc92e8
Raul Roldan <raul.ji@outlook.com>
2022-07-04 20:56:02 -0500

    working on find by artist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -17,27 +17,24 @@ function addToCollection(title,artist,yearPublished) {
     }
    
 
-addToCollection('The black parade', 'My chemical romance', 2007);
-console.log (addToCollection('Brand new eyes', 'Paramore', 2009))
+addToCollection('The black parade.', 'My chemical romance', 2007);
+console.log (addToCollection('Brand new eyes.', 'Paramore', 2009))
 console.log(collection);
 //console.log(addToCollection());
-console.log (addToCollection('Fallen', 'Evanescence', 2003))
-console.log (addToCollection('Wehen we all fall asleep', 'Billie Eilish', 2019))
-console.log (addToCollection('Bon Jovi', 'Bon-Jovi', 1984))
-console.log (addToCollection('Paranoid', 'Black sabbath', 1970))
+console.log (addToCollection('Fallen.', 'Evanescence', 2003))
+console.log (addToCollection('Wehen we all fall asleep.', 'Billie Eilish', 2019))
+console.log (addToCollection('Bon Jovi.', 'Bon-Jovi', 1984))
+console.log (addToCollection('Paranoid.', 'Black sabbath', 1970))
 //console.table(collection);
 console.log('Over all collection', collection);
 
 function showCollection(arrayM) {
+    console.log('Array', arrayM);
     
     for (let i = 0; i < arrayM.length; i++) {
-        let album = arrayM[i];
-        album.title;
-        //if (arrayM[i]) {
-            //console.log(arrayM[i].title + ', ' + arrayM[i].artist + ', ' + arrayM[i].yearPublished + '');
-        //}
-        console.log(album.title);
-        
+        console.log('Title of the Album: ' + arrayM[i].title + ' Artist:' + arrayM[i].artist + ' Year:' + arrayM[i].yearPublished);
+    
     }
+    
 }
 console.log(showCollection(collection));
