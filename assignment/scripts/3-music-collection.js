85aad43d9295beba4aff9ceb08382dae61b65ca8
Raul Roldan <raul.ji@outlook.com>
2022-07-05 11:31:34 -0500

    trying to get findbyartist to work

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+const collection = [];
 let records = {
     name: "Brand new Eyes",
     artist: "Paramore",
@@ -11,7 +11,6 @@ function addToCollection(title,artist,yearPublished) {
     //console.log('Add title of the record',title, 'Add Artist',artist,'Add Year',yearPublished);
     const newrecord={title:title, artist:artist, yearPublished:yearPublished,};
     collection.push(newrecord);
-    
     return newrecord;
 
     }
@@ -25,16 +24,31 @@ console.log (addToCollection('Fallen.', 'Evanescence', 2003))
 console.log (addToCollection('Wehen we all fall asleep.', 'Billie Eilish', 2019))
 console.log (addToCollection('Bon Jovi.', 'Bon-Jovi', 1984))
 console.log (addToCollection('Paranoid.', 'Black sabbath', 1970))
-//console.table(collection);
+console.table(collection);
 console.log('Over all collection', collection);
 
 function showCollection(arrayM) {
-    console.log('Array', arrayM);
+    //console.log('Array', arrayM);
     
     for (let i = 0; i < arrayM.length; i++) {
         console.log('Title of the Album: ' + arrayM[i].title + ' Artist:' + arrayM[i].artist + ' Year:' + arrayM[i].yearPublished);
-    
+        
     }
     
+    return ;
+    // I had a problem with this return? im not sure why it says undefined?
 }
 console.log(showCollection(collection));
+
+
+
+//let artist = '';
+let search = [];
+function findByArtist(artist) { 
+    console.table(artist);
+     artist ='';
+     for (let searchArtist of artist){
+        
+     }
+
+}
